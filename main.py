dc99ba8de8553127a72f85ad023f6a5bc9c14f55
Tom Do <tom@iifuture.com>
2022-07-09 14:51:27 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from multiprocessing.connection import wait
+import pyttsx3
+import time
+from pygame import mixer
+
+
+while True:
+    #get the text needed and convert it to mp3
+    text = input('Enter your text: ')
+    print('saying: ' + text)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.save_to_file(text, 'text.mp3')
+    engine.runAndWait()
+    print('\n')
